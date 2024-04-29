8fd5079699a9c0a972430023d1c6cd60f7d26027
maromasa <mrms23.mb@gmail.com>
2024-04-29 07:56:24 +0000

    Setup Vite project with Typescrit, Add tailwinccss, generaouted.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import App from "../App";
+
+function AppLayout() {
+    return (
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    )
+}
+
+export default AppLayout;
